a7dd90c528ef59f32661c730aa2cd327eaedf351
gurneeshkaur <gurneesh1kaur@gmail.com>
2024-10-18 18:19:50 +0530

    fourth commit

--- base/models.py
+++ base/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser,Group, Permission
 
 
 class User(AbstractUser):
@@ -8,6 +8,9 @@ class User(AbstractUser):
     bio = models.TextField(null=True)
 
     avatar = models.ImageField(null=True, default="avatar.svg")
+
+    groups = models.ManyToManyField(Group, related_name='custom_user_groups', blank=True)
+    user_permissions = models.ManyToManyField(Permission, related_name='custom_user_permissions', blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
